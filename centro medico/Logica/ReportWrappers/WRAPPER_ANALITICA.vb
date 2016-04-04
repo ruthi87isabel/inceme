95214ea372eff4bf48f1e5da5f4aafa6eb7cbe51
ruthi87isabel <ruthi87isabel@gmail.com>
2016-04-04 23:37:02 +0200

    Final Github version to start developing

---
+++ centro medico/Logica/ReportWrappers/WRAPPER_ANALITICA.vb
@@ -0,0 +1,40 @@
+﻿Public Class WRAPPER_ANALITICA
+
+    Private _ANALITICA As centro_medico.CONCEPTOSANALITICA
+
+    Public Sub New(ByVal _ANALITICA As centro_medico.CONCEPTOSANALITICA)
+        Me._ANALITICA = _ANALITICA
+    End Sub
+
+    Public ReadOnly Property CODIGO() As Integer
+        Get
+            Return Me._ANALITICA.CODIGO
+        End Get
+    End Property
+
+    Public ReadOnly Property CONCEPTO() As String
+        Get
+            Return Me._ANALITICA.CONCEPTO
+        End Get
+    End Property
+
+    Public ReadOnly Property IMPORTE() As Single
+
+        Get
+            Return Me._ANALITICA.IMPORTE
+        End Get
+    End Property
+
+    Public ReadOnly Property NOTAS() As String
+        Get
+            Return Me._ANALITICA.NOTAS
+        End Get
+    End Property
+
+    Public ReadOnly Property COLOR() As String
+        Get
+            Return Me._ANALITICA.COLOR
+        End Get
+    End Property
+
+End Class
